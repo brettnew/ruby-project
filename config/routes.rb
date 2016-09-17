09788190bb4d7e65d5327faa8ade1bf4b8c05546
Brett New <brett.new91@gmail.com>
2016-09-16 17:16:27 -0700

    Allow user to create profile with picture.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
 
+  resources :users
+
   resources :languages
 
   root :to => 'users#index'
